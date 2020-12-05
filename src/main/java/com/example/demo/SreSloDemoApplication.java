a1c6cb390753f56cdf6036d66e38a8c0418639ed
Your Name <you@example.com>
2020-12-05 04:59:23 +0100

    Initial Commit

---
+++ src/main/java/com/example/demo/SreSloDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreSloDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreSloDemoApplication.class, args);
+	}
+
+}
