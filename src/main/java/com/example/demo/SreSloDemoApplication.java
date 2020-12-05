8dc7e64d7c908dc28beaf332332715be260173a3
Your Name <you@example.com>
2020-12-05 05:25:50 +0100

    Add micrometer + prometheus dependencies and verify metrics are indeed exposed.

--- src/main/java/com/example/demo/SreSloDemoApplication.java
+++ src/main/java/com/example/demo/SreSloDemoApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SreSloDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SreSloDemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SreSloDemoApplication.class, args);
+  }
 
+  @Timed(percentiles = {0.5, 0.9, 0.99})
+  @GetMapping("hello")
+  public String hello() {
+    return "hello";
+  }
 }
